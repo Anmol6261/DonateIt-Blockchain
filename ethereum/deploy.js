3f5f49b9c5cebcbc633b479c472530a309140085
Anmol Pandita <apandita@atlassian.com>
2023-04-28 22:32:54 +0530

    Fixed routing and deployed again

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -3,7 +3,8 @@ const Web3 = require('web3');
 const compiledFactory= require('./build/CampaignFactory.json');
 
 const provider = new HDWalletProvider(
-  'olive van clog novel uniform twist dial file depth toward tree shove',
+  //'olive van clog novel uniform twist dial file depth toward tree shove'
+  'flat double burst chalk wagon glide bottom amazing law proud cargo unknown',
   // remember to change this to your own phrase!
   'https://sepolia.infura.io/v3/f547f32744d74fa48ddb661bae036dc3'
   // remember to change this to your own endpoint!
@@ -11,6 +12,7 @@ const provider = new HDWalletProvider(
 const web3 = new Web3(provider);
 
 const deploy = async () => {
+  try{
   const accounts = await web3.eth.getAccounts();
 
   console.log('Attempting to deploy from account', accounts[0]);
@@ -22,5 +24,8 @@ const deploy = async () => {
 
   console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
+  } catch(error){
+    console.error('Error deploying contract:', error);
+  }
 };
 deploy();
